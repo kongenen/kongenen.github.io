ba06379cf1a790247d73b4b4a1c7d0b06a734260
k <1687764674@qq.com>
2024-03-20 09:01:42 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
 #npm run build
 
 # 进入生成的文件夹
-#cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 msg='deploy'
 githubUrl=git@github.com:kongenen/kongenen.github.io  # 按你的代码仓库信息进行修改
@@ -15,7 +15,7 @@ githubUrl=git@github.com:kongenen/kongenen.github.io  # 按你的代码仓库信
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl master # 推送到github
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
