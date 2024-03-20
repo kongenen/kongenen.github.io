eaf483abcd6b5d1676bbc5fd131005c7ddcbd4db
k <1687764674@qq.com>
2024-03-20 09:47:21 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 const nav = require('./config/nav.js');
 const htmlModules = require('./config/htmlModules.js');
-const base = require('./config/base.js');
+// const base = require('./config/base.js');
 
 module.exports = {
   title: "Kongen",
   description: '运气交给锦鲤，你只管努力就行。', // 描述,以 <meta> 标签渲染到页面html中
-  base: '/kongenen.github.io/', // '/<github仓库名>/'， 默认'/'
+  base: '/', // '/<github仓库名>/'， 默认'/'
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/milu.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'}],
@@ -56,13 +56,13 @@ module.exports = {
     // },
     
     author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-      name: '麋鹿鲁哟', // 必需
-      link: 'https://github.com/miluluyo' // 可选的
+      name: 'kongen', // 必需
+      link: 'https://github.com/kongenen' // 可选的
     },
     blogger:{ // 博主信息，显示在首页侧边栏
-      avatar: '/vdoingBlog/img/tx.jpg',
-      name: '麋鹿鲁哟',
-      slogan: '前端小菜鸡(✪ω✪)'
+      avatar: '/vdoingBlog/image/1.jpg',
+      name: 'Kongen',
+      slogan: '你好呀(✪ω✪)'
     },
     social:{ // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
@@ -85,8 +85,8 @@ module.exports = {
       ]
     },
     footer:{ // 页脚信息
-      createYear: 2019, // 博客创建年份
-      copyrightInfo: '麋鹿鲁哟 | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      createYear: 2023, // 博客创建年份
+      copyrightInfo: 'Kongen | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
     },
     htmlModules
   },
